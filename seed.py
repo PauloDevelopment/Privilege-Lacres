bc9e63d71c0881a65f4a23d66fab38f16c2c2bb0
PauloDevelopment <ppirescordeiro2017@gmail.com>
2026-04-07 20:20:35 -0300

    Adicionando arquivo seed para criação de admin e bibliotecas no requirements

---
+++ seed.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import pytz
+from werkzeug.security import generate_password_hash
+from db import db
+from main import app
+from model.usuario import Usuario
+
+
+def seed_user():
+    with app.app_context():
+
+        existing_user = Usuario.query.filter_by(email="admin@email.com").first()
+        if existing_user:
+            print("Usuário já existe!")
+            return
+
+        user = Usuario(
+            name="Admin",
+            email="admin@email.com",
+            password=generate_password_hash("123456"),
+            role="admin",
+            data_cadastro=datetime.now(pytz.timezone('America/Sao_Paulo'))
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        print("Usuário admin criado com sucesso!")
+
+
+if __name__ == "__main__":
+    seed_user()
